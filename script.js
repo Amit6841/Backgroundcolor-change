173efb41616558a15df1c5f9131341972e3ecc83
Amit kumar pallai <159411434+Amit6841@users.noreply.github.com>
2025-02-22 23:30:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function backgroundColor(color) {
+    const selectedColor = document.getElementById('colorPicker').value;
+    document.body.style.backgroundColor = selectedColor;
+    document.body.style.backgroundColor = color;
+
+
+}
+
